268cf21209e1796d447f93719e8d8b2de5cc82e5
giancarlovilch <giancarlovilch@gmail.com>
2022-11-06 12:16:49 -0500

    add more

---
+++ test005.php
@@ -0,0 +1,8 @@
+<?php
+    $Edad=32;
+    $Nombre="Oscar";
+    $Altura=1.4;
+    $Programador=null;
+
+Echo "El nombre es ".$Nombre." la edad es ".$Edad." su altura es ".$Altura." el puesto ".$Programador;
+?>
